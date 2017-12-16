49f40f57def2ce190b63e24688c3b40cadcdd43f
sankhala <sankhalayash96@gmail.com>
2017-12-16 14:23:37 +0530

    user authentication

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const schema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            index: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true,
+            index: true,
+            unique: true
+        },
+        passwordHash: { type: String, required: true },
+        confirmed: { type: Boolean, default: false },
+        confirmationToken: { type: String, default: "" }
+    },
+    { timestamps: true }
+);
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+    return bcrypt.compareSync(password, this.passwordHash);
+};
+
+schema.methods.generateJWT = function generateJWT() {
+    return jwt.sign({
+            email: this.email,
+            confirmed: this.confirmed
+        },
+        process.env.JWT_SECRET
+    );
+};
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+    return{
+        email: this.email,
+        confirmed: this.confirmed,
+        token: this.generateJWT()
+    }
+};
+
+export default mongoose.model("User", schema);
