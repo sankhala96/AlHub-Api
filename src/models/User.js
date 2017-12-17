f600cfc25af1d4d21dd7f09235098eb9e0cb03f5
sankhala <sankhalayash96@gmail.com>
2017-12-17 15:20:37 +0530

    confirm mail

--- src/models/User.js
+++ src/models/User.js
@@ -33,6 +33,14 @@ schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`
+};
+
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
             email: this.email,
